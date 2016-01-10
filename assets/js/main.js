41ee6bdabcc590ad21efe7967d3710358cc9008e
Huseyin M Ozyilmaz <huseyinozyilmaz@users.noreply.github.com>
2016-01-10 15:14:06 +0000

    clears all the form inputs after successful send

--- assets/js/main.js
+++ assets/js/main.js
@@ -36,6 +36,7 @@ $(document).ready(function(){
                     type: "success",   
                     confirmButtonText: "OK"
                 });
+                $("#contact-form").trigger("reset");
             }
             else {
                 swal({
